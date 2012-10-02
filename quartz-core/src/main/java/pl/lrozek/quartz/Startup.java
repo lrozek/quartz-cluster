74f00357aa79cbc255d4ff4b8eceaddc880a17ff
Lukasz Rozek <lukasz.rozek@consileon.pl>
2012-10-02 18:45:04 +0200

    jobs added at runtime

--- quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
+++ quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
@@ -1,12 +1,26 @@
 package pl.lrozek.quartz;
 
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SimpleTrigger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import pl.lrozek.quartz.job.DynamicJob;
 
 public class Startup {
 
     public static void main( String[] args ) throws Exception {
         ApplicationContext applicationContext = new GenericXmlApplicationContext( "classpath:application-context.xml" );
+        Scheduler scheduler = applicationContext.getBean( Scheduler.class );
+        for ( int i = 1; i <= 10; i++ ) {
+            SimpleTrigger simpleTrigger = newTrigger().withSchedule( simpleSchedule().withIntervalInSeconds( 1 ).withRepeatCount( 50 ) ).build();
+            JobDetail jobDetail = JobBuilder.newJob( DynamicJob.class ).withIdentity( "dynamicJob" + i ).usingJobData( "unternehmenId", Long.valueOf( i ) ).build();
+            scheduler.scheduleJob( jobDetail, simpleTrigger );
+        }
     }
-
 }
--- quartz-core/src/main/java/pl/lrozek/quartz/job/DynamicJob.java
+++ quartz-core/src/main/java/pl/lrozek/quartz/job/DynamicJob.java
@@ -6,16 +6,19 @@ import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
-public class SimpleJob extends QuartzJobBean {
+@Component
+public class DynamicJob extends QuartzJobBean {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger( SimpleJob.class );
+    private static final Logger LOGGER = LoggerFactory.getLogger( DynamicJob.class );
 
     @Override
     protected void executeInternal( JobExecutionContext context ) throws JobExecutionException {
         try {
+            Long unternehmenId = (Long) context.getJobDetail().getJobDataMap().get( "unternehmenId" );
             String instanceId = context.getScheduler().getSchedulerInstanceId();
-            LOGGER.info( "running simple job in following instance: {}", instanceId );
+            LOGGER.info( "running dynamic job for following unternehmenId: {} in following instance {}", unternehmenId, instanceId );
             Thread.sleep( 10000 );
         }
         catch ( SchedulerException e ) {
@@ -25,4 +28,5 @@ public class SimpleJob extends QuartzJobBean {
             LOGGER.error( "exception thrown", e );
         }
     }
+
 }
