73edcb7bf81d92f70796ab2e617dd5b90f6a5325
Lukasz Rozek <lukasz.rozek@consileon.pl>
2012-10-02 11:36:22 +0200

    quartz jobstore in db

--- quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
+++ quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
@@ -1,7 +1,5 @@
 package pl.lrozek.quartz;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -9,8 +7,6 @@ public class Startup {
 
     public static void main( String[] args ) throws Exception {
         ApplicationContext applicationContext = new GenericXmlApplicationContext( "classpath:application-context.xml" );
-        DataSource dataSource = applicationContext.getBean( DataSource.class );
-        dataSource.getConnection();
     }
 
 }
