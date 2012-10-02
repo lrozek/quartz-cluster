e6153d77790cfceca8f2fb778d28b54862f0cd89
Lukasz Rozek <lukasz.rozek@consileon.pl>
2012-10-02 11:04:46 +0200

    building data source for quartz

--- quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
+++ quartz-core/src/main/java/pl/lrozek/quartz/Startup.java
@@ -1,14 +1,16 @@
 package pl.lrozek.quartz;
 
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Startup {
 
-    public static void main( String[] args ) throws SchedulerException {
-        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        scheduler.start();
+    public static void main( String[] args ) throws Exception {
+        ApplicationContext applicationContext = new GenericXmlApplicationContext( "classpath:application-context.xml" );
+        DataSource dataSource = applicationContext.getBean( DataSource.class );
+        dataSource.getConnection();
     }
 
 }
